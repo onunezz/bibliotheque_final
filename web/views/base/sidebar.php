af79a00453ef471f5575a22e2ea30fdd3e0e944d
onunezz <alegusnunez@gmail.com>
2024-07-25 05:28:50 -0300

    acomodando distintas partes

--- web/views/base/sidebar.php
+++ web/views/base/sidebar.php
@@ -1,6 +1,5 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
     <div class="sidebar-brand d-flex align-items-center justify-content-center">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-book-open"></i>
@@ -8,26 +7,17 @@
         <div class="sidebar-brand-text mx-3">bblthq</div>
     </div>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+    <hr class="sidebar-divider mb-0">
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+    <li class="nav-item margin-nav-item">
+        <a class="nav-link" href="index.php?pages=home">
             <i class="fas fa-fw fa-home"></i>
             <span>Inicio</span></a>
     </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    <hr class="sidebar-divider mb-0">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Interface
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
+    <li class="nav-item margin-nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-book"></i>
             <span>Libros</span>
@@ -42,47 +32,37 @@
         </div>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item margin-nav-item">
         <a class="nav-link" href="#">
             <i class="fas fa-fw fa-file-export"></i>
             <span>Préstamos</span></a>
     </li>
 
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    <hr class="sidebar-divider mb-0">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
+    <li class="nav-item margin-nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
             <i class="fas fa-fw fa-wrench"></i>
             <span>Administración</span>
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
+                <h6 class="collapse-header">Gestión de usuarios:</h6>
                 <a class="collapse-item" href="#">Usuarios</a>
                 <a class="collapse-item" href="#">Mis datos</a>
             </div>
         </div>
     </li>
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
+    <li class="nav-item margin-nav-item">
         <a class="nav-link" href="charts.html">
             <i class="fas fa-fw fa-download"></i>
             <span>Reporte</span></a>
     </li>
 
-    <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
--- web/views/pages/home.php
+++ web/views/pages/home.php
@@ -1,10 +1,19 @@
-<h1>bibliotheque</h1>
-<h2>Administración de libros y préstamos</h2>
-<?php
+<section class="container">
+    <div class="row justify-content-center mt-5 pt-5">
+        <div class="col-xl-6 col-lg-6 col-md-9">
 
-require_once __DIR__ . '/../../config/MysqlDb.php'; // Asegúrate de poner la ruta correcta a tu archivo MysqlDb.php
+            <h1 class="h1-home">bibliotheque</h1>
+            <h2>Administración y préstamos de libros</h2>
+            <?php
 
-// Llama al método testConnection
-MysqlDb::testConnection();
+            require_once __DIR__ . '/../../config/MysqlDb.php'; // Asegúrate de poner la ruta correcta a tu archivo MysqlDb.php
 
-?>
+            // Llama al método testConnection
+            MysqlDb::testConnection();
+
+            ?>
+        </div>
+
+    </div>
+
+</section>
