7be8f55fc49e5dc88d080ed021977db1f48026bc
Alejandro Nunez <alegusnunez@gmail.com>
2024-07-26 19:11:58 -0300

    fuente-sin-link

--- web/views/base/head.php
+++ web/views/base/head.php
@@ -3,7 +3,7 @@
 <title>bibliotheque</title>
 <link rel="icon" type="image/x-icon" href="img/favicon.png">
 <link href="public/plugins/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link href="public/css/nunito.css" rel="stylesheet">
 <link rel="stylesheet" href="public/css/sb-admin-2.min.css">
 <link href="public/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 <link rel="stylesheet" href="public/plugins/sweetalert2/sweetalert2.min.css">
