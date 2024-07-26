22b8690eddc8d56d36a3dcfb2dada4b0930575f7
onunezz <alegusnunez@gmail.com>
2024-07-26 17:41:41 -0300

    alertas y listeditorials completo

--- web/models/EditorialModel.php
+++ web/models/EditorialModel.php
@@ -38,4 +38,70 @@ class EditorialModel
             print_r($stmt->errorInfo());
         }
     }
+
+    static public function checkDuplicates($value1)
+    {
+        try {
+            $query = "SELECT COUNT(*) FROM editorials WHERE name = ?;";
+            $checkStatement = MysqlDb::connect()->prepare($query);
+            $checkStatement->bindParam(1, $value1, PDO::PARAM_STR);
+            $checkStatement->execute();
+
+            $count = $checkStatement->fetchColumn();
+
+            if ($count > 0) {
+                return true;
+            }
+
+            return false;
+        } catch (PDOException $e) {
+            echo "Error en la validación de duplicados: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public static function checkDuplicatesForUpdate($name, $id)
+    {
+        $stmt = MysqlDb::connect()->prepare("SELECT COUNT(*) FROM editorials WHERE name = :name AND id != :id");
+        $stmt->bindParam(":name", $name, PDO::PARAM_STR);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchColumn() > 0 ? true : false;
+    }
+
+    public static function updateEditorial($id, $name)
+    {
+        $stmt = MysqlDb::connect()->prepare("UPDATE editorials SET name = :name WHERE id = :id");
+        $stmt->bindParam(":name", $name, PDO::PARAM_STR);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public static function disableEditorial($id)
+    {
+        $sql = "UPDATE editorials SET state = 2 WHERE id = ?;";
+        $stmt = MysqlDb::connect()->prepare($sql);
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            print_r($stmt->errorInfo());
+            return false;
+        }
+        $stmt = null;
+    }
+
+    public static function enableEditorial($id)
+    {
+        $sql = "UPDATE editorials SET state = 1 WHERE id = ?;";
+        $stmt = MysqlDb::connect()->prepare($sql);
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            print_r($stmt->errorInfo());
+            return false;
+        }
+        $stmt = null;
+    }
 }
